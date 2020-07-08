924a5a732279d9ca4cd3fe2190f975afd470cc31
Cristian Turcios <cristian.turcios@laureate.net>
2020-07-08 13:19:19 -0600

    remove azure keys

--- config/plugins.js
+++ config/plugins.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     provider: 'azure-storage',
     providerOptions: {
       account: 'infosecreport', //env('STORAGE_ACCOUNT'),
-      accountKey: 'iMve8VCcir9xhK6K3kl3ct+wcnB0dS1yeWsbmeeSePiuxoMo330iWFSXGQSLVG3BHxlcs2CiZchzisUbB8LJVA==', //env('STORAGE_ACCOUNT_KEY'),
+      accountKey: '', //env('STORAGE_ACCOUNT_KEY'),
       serviceBaseURL: '',//env('STORAGE_URL'),
       containerName: 'strapi', //env('STORAGE_CONTAINER_NAME'),
       defaultPath: 'assets',
