c3eecabb8b2b4992631c0ca0632ba25099826c48
Cristian Turcios <cristian.turcios@laureate.net>
2020-07-08 13:43:28 -0600

    added heroku postgress configuration

--- config/database.js
+++ config/database.js
@@ -1,18 +1,18 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-});
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
 // module.exports = ({ env }) => ({
 //   defaultConnection: 'default',
 //   connections: {
@@ -31,3 +31,24 @@ module.exports = ({ env }) => ({
 //     },
 //   },
 // });
+// Heroku Postgress
+module.exports = ({
+  env
+}) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'ec2-34-224-229-81.compute-1.amazonaws.com'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'dd1bfan8k344e0'),
+        username: env('DATABASE_USERNAME', 'vngygqixyiwmrk'),
+        password: env('DATABASE_PASSWORD', 'ad9d6cd269250c9f30a4e87d67463dcf6f33a24a6e3f63537e08f137ed08b690'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    },
+  },
+});
